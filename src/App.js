4f1049e26e18bf779c37b00f5635d5b1c18127a2
Dorson Williams <clearhillpc@gmail.com>
2022-10-12 08:15:04 +0000

    initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import './App.css';
+import Tweet from './Tweet'
+
+function App() {
+  return (
+    <Tweet text = 'passed down text' author ='Paul Mahooney' date='Texas'/>
+  );
+}
+
+export default App;
---
+++ src/Tweet.js
@@ -0,0 +1,26 @@
+import React, { useState } from 'react'
+
+const Tweet = (props) => {
+
+    const [counter, setCounter] = useState(0)
+
+    const increment = () => {
+        setCounter( counter + 4)
+    }
+
+
+    const text = props.text
+    const author = props.author
+    const date = props.date
+    return (
+        <div>
+            <h3>{text.toUpperCase()}</h3> 
+            <h6>{date.toUpperCase()} by {author.toUpperCase()}</h6>
+            <br></br>
+            <h1>counter: {counter}</h1>
+            <button onClick={increment}>+</button>
+        </div>
+    )
+}
+
+export default Tweet;
